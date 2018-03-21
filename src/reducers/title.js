6a00c9c0909a8657ea9ce173b3ef8b64cd605760
Paul Craig <paul_craig_16@hotmail.com>
2018-03-21 05:43:23 -0400

    Add <title>s
    Up until now, it was just showing a default that wasn't relevant.

--- src/reducers/title.js
+++ src/reducers/title.js
@@ -1,9 +1,17 @@
-export default (state = 'EnerGuide API', action = {}) => {
+export default (state = 'EnerGuide API Proof of Concept', action = {}) => {
   switch (action.type) {
     case 'HOME':
-      return 'Home'
+      return `Home | EnerGuide API Proof of Concept`
+    case 'SEARCH':
+      return `Search | EnerGuide API Proof of Concept`
+    case 'LOCATION':
+      return `Search by location | EnerGuide API Proof of Concept`
+    case 'FILEID':
+      return `Search by file ID | EnerGuide API Proof of Concept`
+    case 'RESULTS':
+      return `Results for location | EnerGuide API Proof of Concept`
     case 'RESULTSFILEID':
-      return 'Results for file ID'
+      return 'Results for file ID | EnerGuide API Proof of Concept'
     default:
       return state
   }
