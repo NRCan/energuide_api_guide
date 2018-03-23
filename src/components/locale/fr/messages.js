8bf61d43d205efd67a459c1825fc784f7dd2edef
Paul Craig <paul_craig_16@hotmail.com>
2018-03-23 00:29:53 -0400

    New batch of translations

--- src/components/locale/fr/messages.js
+++ src/components/locale/fr/messages.js
@@ -6,22 +6,14 @@ module.exports = {
     },
   },
   m: {
-    'Get access': 'Avoir acc\xE8s',
-    'No results': 'Aucun r\xE9sultat',
-    'Search for another file <0>ID</0>':
-      'Rechercher un autre fichier <0> ID </ 0>',
-    results: 'r\xE9sultats',
-    'Search again by location': 'Rechercher \xE0 nouveau par emplacement',
-    'To test the API, you are able to search by Location or File <0>ID</0>.':
-      "Pour tester l'API, vous pouvez effectuer une recherche par emplacement ou par fichier <0> ID </ 0>.",
-    'Search by Location or File <0>ID</0>':
-      'Recherche par emplacement ou fichier <0> ID </ 0>',
     Alpha: 'Alpha',
-    'This is an internal service.': 'Ceci est un service interne',
-    'To provide feedback, email us':
-      'Pour envoyer vos commentaires, envoyez-nous un e-mail',
+    'This is a demonstration of how the API could be used.':
+      "Ceci est une d\xE9monstration de la fa\xE7on dont l'API pourrait \xEAtre utilis\xE9e",
+    'Help us improve this internal service, email your feedback':
+      'Aidez-nous \xE0 am\xE9liorer ce service interne, envoyez-nous vos commentaires',
     Privacy: 'Confidentialit\xE9',
     'Ready to use the Energuide API?': "Pr\xEAt \xE0 utiliser l'API EnerGuide?",
+    'Get access': 'Avoir acc\xE8s',
     'Natural Resources Canada': 'Ressources naturelles Canada',
     'EnerGuide API': "L'API EnerGuide",
     'The EnerGuide API allows you to access the housing data provided by Natural Resources Canada. You will have open access the data, and will be able to query to an individual dwelling.':
@@ -40,7 +32,7 @@ module.exports = {
     'You can choose to access the granular data that you need.':
       "Vous pouvez choisir d'acc\xE9der aux donn\xE9es granulaires dont vous avez besoin.",
     'Access current data': 'Acc\xE9der aux donn\xE9es actuelles',
-    'As new housing evaluations are added to the EnerGuide API, the data will be updated and available for you to use.':
+    'As more housing evaluations are added to the EnerGuide API, the data will be updated and available for you to use.':
       "\xC0 mesure que de nouvelles \xE9valuations de logement sont ajout\xE9es \xE0 l'API \xC9nerGuide, les donn\xE9es seront mises \xE0 jour et mises \xE0 votre disposition.",
     'How can I use the EnerGuide API?':
       "Comment puis-je utiliser l'API \xC9nerGuide?",
@@ -54,10 +46,18 @@ module.exports = {
     'view the EnerGuide API documentation':
       "voir la documentation de l'API EnerGuide",
     'on GitHub.': 'sur GitHub',
+    'No results': 'Aucun r\xE9sultat',
+    'Search for another file <0>ID</0>': 'Rechercher un autre fichier ID',
     'File <0>ID</0>': 'Fichier ID',
     'Search by file <0>ID</0>': 'Recherche par fichier ID ',
+    results: 'r\xE9sultats',
+    'Search again by location': 'Rechercher \xE0 nouveau par emplacement',
     'Which parameter would you like to search by?':
       'Quel param\xE8tre souhaitez-vous rechercher?',
+    'To test the API, you are able to search by Location or File <0>ID</0>.':
+      "Pour tester l'API, vous pouvez effectuer une recherche par emplacement ou par fichier ID.",
+    'Search by Location or File <0>ID</0>':
+      'Recherche par emplacement ou fichier ID',
     Location: 'Location',
     'Search by the file <0>ID</0> on your Energuide evaluation.':
       'Recherchez par le fichier ID sur votre \xE9valuation Energuide.',
@@ -74,48 +74,5 @@ module.exports = {
     All: 'tout',
     Home: 'Domicile',
     'Thank you': 'Merci',
-    'Get Access': 'Acc\xE9der',
-    'To test the API, you are able to search by Location or File number.':
-      "Pour tester l'API, vous pouvez effectuer une recherche par emplacement ou par num\xE9ro de fichier.",
-    'Search by Location or File number':
-      'Recherche par lieu ou num\xE9ro de fichier.',
-    'File number': 'Num\xE9ro de fichier',
-    '<0>Alpha</0>This is an internal service. <1>To provide feedback, email us</1>.':
-      'Alpha Ceci est un service interne. Pour nous faire part de vos commentaires, envoyez-nous un e-mail.',
-    'Pour voir toutes les donn\xE9es disponibles,':
-      'Pour voir toutes les donn\xE9es disponibles,',
-    'The EnerGuide API allows you to access the housing data provided by Natural Resource Canada. You will have open access to the data, with granularity to search to a individual dwelling level.':
-      "L'API \xC9nerGuide vous permet d'acc\xE9der aux donn\xE9es sur le logement fournies par Ressources naturelles Canada. Vous aurez un acc\xE8s ouvert aux donn\xE9es, avec une granularit\xE9 pour rechercher un niveau de logement individuel.",
-    'An <0>API</0>, or <1>Application Programming Interface</1>, is a set of routines, protocols and tools for building software. An API makes it easier to develop a computer program by providing all the building blocks, which are then put together by the programmer.':
-      "Une interface de programmation d'application, ou API, est un ensemble de routines, de protocoles et d'outils pour cr\xE9er des logiciels. Une API facilite le d\xE9veloppement d'un programme informatique en fournissant tous les blocs de construction, qui sont ensuite mis en place par le programmeur.",
-    'To learn how to use the API, we have provided the documentation on Github. The documentation will show all of the data types available under the EnerGuide API.':
-      "Pour apprendre \xE0 utiliser l'API, nous avons fourni la documentation sur Github. La documentation montrera tous les types de donn\xE9es disponibles dans l'API \xC9nerGuide.",
-    'Search by the type of energy source. Choose the parameter that applies.':
-      "Recherche par type de source d'\xE9nergie. Choisissez le param\xE8tre qui s'applique.",
-    Any: 'Tout',
-    'To see how it can be used,':
-      'Pour voir comment il peut \xEAtre utilis\xE9',
-    'test the API': "testez l'API",
-    'Alpha - This is an internal service':
-      'Alpha - Ceci est un service interne',
-    'What is the EnerGuide API?': "Qu'est-ce que l'API EnerGuide?",
-    'Access the API': "Acc\xE9der \xE0 l'API",
-    'An <0>Application Programming Interface</0>, or<1>API</1>, is a set of routines, protocols and tools for building software. Many applications can talk to each other and deliver a response.':
-      "Une <0> Application Programming Interface </ 0>, ou <1> API </ 1>, est un ensemble de routines, de protocoles et d'outils pour cr\xE9er des logiciels. De nombreuses applications peuvent se parler et fournir une r\xE9ponse.",
-    'View open data': 'Afficher les donn\xE9es ouvertes',
-    'The EnerGuide API will help make energy consumption data accessible for everyone.':
-      "L'API \xC9nerGuide aidera \xE0 rendre les donn\xE9es sur la consommation d'\xE9nergie accessibles \xE0 tous.",
-    'You can choose to access the granular data that you need. You will benefit from the ability to extract data data that is specific to your needs.':
-      "Vous pouvez choisir d'acc\xE9der aux donn\xE9es granulaires dont vous avez besoin. Vous b\xE9n\xE9ficierez de la capacit\xE9 d'extraire des donn\xE9es de donn\xE9es sp\xE9cifiques \xE0 vos besoins.",
-    'Developers and data analysts can allow users to query specific information from large data sets. They set the rules that will guide the user to what they need.':
-      "Les d\xE9veloppeurs et les analystes de donn\xE9es peuvent permettre aux utilisateurs d'interroger des informations sp\xE9cifiques \xE0 partir de grands ensembles de donn\xE9es. Ils d\xE9finissent les r\xE8gles qui guideront l'utilisateur \xE0 ce dont ils ont besoin.",
-    'Read the documentation': 'Lire la documentation',
-    'Search by': 'Recherch\xE9 par',
-    'Search by Location': 'Recherche par lieu',
-    'Search by the type of energy source. Choose all of the parameters that apply.':
-      "Recherche par type de source d'\xE9nergie. Choisissez tous les param\xE8tres qui s'appliquent.",
-    'An <0>Application Programming Interface</0>Application Programming Interface, or<1>API</1>, is a set of routines, protocols and tools for building software. Many applications can talk to each other and deliver a response.':
-      "Une <0> interface de programmation d'application </ 0> Application Programming Interface, ou <1> API </ 1>, est un ensemble de routines, de protocoles et d'outils pour la construction de logiciels. De nombreuses applications peuvent se parler et fournir une r\xE9ponse.",
-    HOME: 'Home',
   },
 }
