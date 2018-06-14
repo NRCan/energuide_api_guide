8d35e62690cc889c2143e007211abf6f75c3625c
Mike Williamson <mike.williamson@cds-snc.ca>
2018-06-13 21:35:10 -0400

    Set viewport size

--- server/render.js
+++ server/render.js
@@ -25,6 +25,7 @@ export default ({ clientStats }) => async (req, res, next) => {
       <html lang="en">
         <head>
           <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
           <title>${state.title}</title>
           ${styles}
           <style type="text/css">
